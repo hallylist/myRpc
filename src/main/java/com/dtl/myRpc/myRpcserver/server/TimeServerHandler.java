75a504b11c012377ebb2b5b9257ceffc01ae8a1b
dongtianlong <dongtianlong@lightinthebox.com>
2018-11-01 18:08:04 +0800

    modify server bytebuf

--- src/main/java/com/dtl/myRpc/myRpcserver/server/TimeServerHandler.java
+++ src/main/java/com/dtl/myRpc/myRpcserver/server/TimeServerHandler.java
@@ -2,34 +2,33 @@ package com.dtl.myRpc.myRpcserver.server;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
 public class TimeServerHandler extends ChannelInboundHandlerAdapter {
-//    @Override
-//    public void channelActive(ChannelHandlerContext ctx) {
-//        final ByteBuf time = ctx.alloc().buffer(4);
-//        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
-//
-//        final ChannelFuture f = ctx.writeAndFlush(time);
-//        f.addListener((ChannelFutureListener) future -> {
-//            assert f == future;
-////            ctx.close();
-//        });
-//    }
+    private ByteBuf buf;
+    int index = 0;
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg){
         ByteBuf in=(ByteBuf)msg;
-        System.out.println("server receive from client: "+in.toString(CharsetUtil.UTF_8));
-        ctx.writeAndFlush(Unpooled.copiedBuffer("hello client".getBytes()));
-//        f.addListener(future -> {
-//            assert f==future;
-//            in.release();
-////            ctx.close();
-//        });
+        if(buf==null){
+            buf = ctx.alloc().buffer(in.readableBytes());
+            in.readBytes(buf);
+        }else {
+            ByteBuf bufTem=ctx.alloc().buffer(buf.readableBytes()+in.readableBytes());
+            buf.readBytes(bufTem,bufTem.writerIndex(),buf.readableBytes());
+            in.readBytes(bufTem,bufTem.writerIndex(),in.readableBytes());
+            buf.release();
+            buf = bufTem;
+        }
+        while (buf.readableBytes()>12) {
+            System.out.println("server receive from client: "+buf.readBytes(12).toString(CharsetUtil.UTF_8));
+            System.out.println(index++);
+            ctx.writeAndFlush(Unpooled.copiedBuffer("hello client".getBytes()));
+        }
+
     }
 
     @Override
