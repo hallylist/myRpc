bf62172ba62ed19700a4b3b3a45988bcd1d9352a
dongtianlong <dongtianlong@lightinthebox.com>
2018-11-02 17:57:09 +0800

    modify server bytebuf

--- src/main/java/com/dtl/myRpc/myRpcserver/server/TimeServerHandler.java
+++ src/main/java/com/dtl/myRpc/myRpcserver/server/TimeServerHandler.java
@@ -7,32 +7,39 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
 public class TimeServerHandler extends ChannelInboundHandlerAdapter {
-    private ByteBuf buf;
+    private String buf = "";
+    private int readIndex = 0;
     int index = 0;
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg){
-        ByteBuf in=(ByteBuf)msg;
-        if(buf==null){
-            buf = ctx.alloc().buffer(in.readableBytes());
-            in.readBytes(buf);
-        }else {
-            ByteBuf bufTem=ctx.alloc().buffer(buf.readableBytes()+in.readableBytes());
-            buf.readBytes(bufTem,bufTem.writerIndex(),buf.readableBytes());
-            in.readBytes(bufTem,bufTem.writerIndex(),in.readableBytes());
-            buf.release();
-            buf = bufTem;
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        if ("".equals(buf)) {
+            circurRead(ctx, in);
+        } else {
+            System.out.println("server receive from client: " + buf + in.toString(0, 12-readIndex, CharsetUtil.UTF_8));
+            in.readerIndex(12-readIndex);
+            System.out.println(index++);
+            readIndex = 0;
+            buf = "";
+            circurRead(ctx, in);
         }
-        while (buf.readableBytes()>12) {
-            System.out.println("server receive from client: "+buf.readBytes(12).toString(CharsetUtil.UTF_8));
+    }
+
+    private void circurRead(ChannelHandlerContext ctx, ByteBuf in) {
+        while (in.readableBytes() >= 12) {
+            System.out.println("server receive from client: " + in.readBytes(12).toString(CharsetUtil.UTF_8));
             System.out.println(index++);
             ctx.writeAndFlush(Unpooled.copiedBuffer("hello client".getBytes()));
         }
-
+        if (in.readableBytes() > 0) {
+            readIndex = in.readableBytes();
+            buf += in.toString(CharsetUtil.UTF_8);
+        }
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause){
         cause.printStackTrace();
         ctx.close();
     }
